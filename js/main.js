07054a9f53156139c8e7e23b903794c231d4033c
jfr0st <jfrosst@yandex.ru>
2024-04-12 11:13:04 +0300

    Начальный коммит

---
+++ js/main.js
@@ -0,0 +1,66 @@
+// document.write("JavaScript говорит привет!");
+// console.log("JavaScript говорит привет!")
+// console.info("JavaScript говорит привет!")
+// console.error("JavaScript говорит привет!")
+// console.warn("JavaScript говорит привет!")
+
+// var num;
+// num = 5;
+
+// var num = 5;
+// num = 7;
+// console.log("Переменная: " + num + ".")
+// const num2 = 5;
+// // num2 = 7;
+//
+// var number;
+// number = 5;
+// number = 5.5;
+// number = "Words";
+// number = 'Word';
+// number = true;
+// console.log(number)
+
+// var num_1 = 5;
+// var num_2 = "5";
+// var num_3 = num_1+ num_2;
+// console.log(num_3)
+
+
+// var num_1 = 5;
+// var num_2 = 15;
+// var res =  num_2 - num_1
+// console.log("Вычитание: " + res)
+// console.log("Вычитание: " + (num_1 - num_2))
+//
+// var num_3 = 5;
+// // num_3 = num_3 + 7;
+// num_3 += 7;
+// console.log("Результат: " + num_3)
+// num_3++;
+// console.log("Результат: " + num_3)
+// num_3--;
+// console.log("Результат: " + num_3)
+//
+// var str_1= "12";
+// var str_2= "2";
+// console.log("Результат: " + (str_1 + str_2))
+// console.log("Результат: " + (Number(str_1) + Number(str_2)))
+//
+// console.log("Math: " + Math.PI);
+// console.log("Math: " + Math.E);
+// console.log("Math: " + Math.min(4,7,6,8,1,2,7,-5));
+// console.log("Math: " + Math.max(4,7,6,8,1,2,7,-5));
+
+var number2 = 15;
+if (number2 == 5) {
+    console.log("OK")
+} else if (number2 < 10) {
+    console.log("OK!")
+} else if (number2 == 7) {
+    console.log("7!")
+} else if (number2 >= 15) {
+    console.log(">= 15")
+} else {
+    console.log("Else!")
+}
